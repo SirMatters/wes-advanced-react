b87ded6e582d0311841f7d20d8f32359c7bc64c5
SirMatters <perov.krll@gmail.com>
2021-02-17 00:15:03 +0300

    Add auth to the app

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,34 +1,53 @@
 import { config, createSchema } from '@keystone-next/keystone/schema';
 import 'dotenv/config';
-import { User } from './schemas/user';
+import { createAuth } from '@keystone-next/auth';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
 
 const databasUrl =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
-const sesionConfig = {
+const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360, // how long should they stay signed in
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: add initial roles here
   },
-  db: {
-    adapter: 'mongoose',
-    url: databasUrl,
-    // TODO: add data seeding
-  },
-  lists: createSchema({
-    // schema items go here
-    User,
-  }),
-  ui: {
-    // TODO: change this for roles
-    isAccessAllowed: () => true,
-  },
-  // TODO: add sessionvalues here
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databasUrl,
+      // TODO: add data seeding
+    },
+    lists: createSchema({
+      // schema items go here
+      User,
+    }),
+    ui: {
+      // show the UI only for those who pass the test
+      isAccessAllowed: ({ session }) => !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id',
+    }),
+  })
+);
