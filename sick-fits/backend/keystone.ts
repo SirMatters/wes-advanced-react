33ecd5c5bef3bf58f307a83fcee263f9feb95e42
SirMatters <perov.krll@gmail.com>
2021-02-16 23:48:56 +0300

    crate Users schema & import to keystone

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,5 +1,6 @@
 import { config, createSchema } from '@keystone-next/keystone/schema';
 import 'dotenv/config';
+import { User } from './schemas/user';
 
 const databasUrl =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -23,6 +24,7 @@ export default config({
   },
   lists: createSchema({
     // schema items go here
+    User,
   }),
   ui: {
     // TODO: change this for roles
